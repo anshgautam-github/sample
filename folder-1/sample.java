8c3f37217d46fdf9cfad39eaa1fe541019116787
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2024-12-23 19:13:27 +0530

    Update sample.java
    adding the content here

--- folder-1/sample.java
+++ folder-1/sample.java
@@ -1,1 +1,13 @@
 
+// single line comments
+
+/*
+  this is  a multi line
+  comment.
+*/
+
+class sample {
+    public static void main(String[] args) {
+        System.out.println("Sample");
+    }
+}
